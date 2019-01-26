f6b1754f670993c97c60690e3e58f46dff02b9b1
romaroman <chaban.roman@protonmail.com>
2019-01-26 16:26:57 +0200

    changes

--- visulast/bot.py
+++ visulast/bot.py
@@ -1,7 +1,7 @@
 import os
 import telegram
 
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, ConversationHandler, CommandHandler, RegexHandler
 from config import CONFIGURATION
 from logger import get_logger
 import controllers
@@ -10,13 +10,15 @@ from functools import wraps
 
 logger = get_logger(os.path.basename(__file__))
 
-updater = Updater(token=CONFIGURATION.telegram_bot)
-dispatcher = updater.dispatcher
+PERIOD = 1
+
+
+# noinspection PyUnusedLocal
+def start(bot, update):
+    update.message.reply_text('Hi! I am your personal last.fm plotter')
 
 
 def send_action(action):
-    """Sends `action` while processing func command."""
-
     def decorator(func):
         @wraps(func)
         def command_func(*args, **kwargs):
@@ -25,11 +27,7 @@ def send_action(action):
             return func(bot, update, **kwargs)
 
         return command_func
-
     return decorator
-
-
-send_upload_photo_action = send_action(telegram.ChatAction.UPLOAD_PHOTO)
 
 
 @send_action(telegram.ChatAction.UPLOAD_PHOTO)
@@ -42,6 +40,7 @@ def artists(bot, update, args):
         file = controller.artist_amount_world_map(int(args[2]))
     bot.send_photo(chat_id=update.message.chat_id, caption='Your map bro)',
                    photo=open(file, 'rb'))
+    return PERIOD
     # bot.send_message(chat_id=update.message.chat_id, text="Sending photo!")
 
 
@@ -51,21 +50,33 @@ def default_username(bot, update, args):
     bot.send_message(chat_id=update.message.chat_id, text=update.message.chat_id)
 
 
-def period_keyboard(bot, update):
+def period(bot, update):
     custom_keyboard = [['today', 'week', 'month'],
                        ['quartal', 'half of year', 'year'],
                        ['overall', 'custom']]
-    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
-    bot.send_message(chat_id=update.message.chat_id, text="Custom Keyboard Test", reply_markup=reply_markup)
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard, one_time_keyboard=True)
+    bot.send_message(chat_id=update.message.chat_id, text="Choose interval", reply_markup=reply_markup)
 
 
-artists_handler = CommandHandler('artists', artists, pass_args=True)
-dispatcher.add_handler(artists_handler)
+def main():
+    updater = Updater(token=CONFIGURATION.telegram_bot)
+    dispatcher = updater.dispatcher
 
-default_username_handler = CommandHandler('default_username', default_username, pass_args=True)
-dispatcher.add_handler(default_username_handler)
+    handlers = [
+        ConversationHandler(
+            entry_points=[CommandHandler('start', start)],
+            states={
+                PERIOD: [RegexHandler('^(Day|Week|Month|Quarter|Half a year|Year|Overall|Custom$', period)]
+            }
+        ),
+        CommandHandler('artists', artists, pass_args=True),
+        CommandHandler('default_username', default_username, pass_args=True),
+    ]
+    for handler in handlers:
+        dispatcher.add_handler(handler)
 
-keyboard_handler = CommandHandler('keyboard', period_keyboard)
-dispatcher.add_handler(keyboard_handler)
+    updater.start_polling()
 
-updater.start_polling()
+
+if __name__ == '__main__':
+    main()
