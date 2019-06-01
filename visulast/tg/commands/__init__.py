5542d30ff5a9e424cfb1a3ca1be9dde38a824c0a
romaroman <chaban.roman@protonmail.com>
2019-06-01 16:28:41 +0300

    again refactored files structure

--- visulast/tg/commands/__init__.py
+++
@@ -1,6 +0,0 @@
-from .general import abort, authenticate, authorize, cancel, clean, check_username, donate, error_callback, get_faq,\
-    get_help, report
-from .visualize import visualize, period_choosing, subject_type_choosing, user_subject_choosing, graph_choosing,\
-    lastfm_subject_choosing,  wrong_response
-from .configure import configure
-from .start import start
